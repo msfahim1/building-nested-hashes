aa6bb51d621e842cf82f42df6c6dc7df6350244c
Michael Fahim <michael_fahim@yahoo.com>
2019-12-07 21:09:46 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,28 +3,42 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
+        name: "Lord Montague", age: "53",
+
         },
       :matriarch => {
         #your key/value pairs here
+        name: "lady montague", age:"54"
         },
       :hero => {
+        name:"Romeo" , age:"15", status: "alive"
         #your key/value pairs here
         },
-      :hero_friends => []
-   }, 
+      :hero_friends => [
+        {name: "benvolio", age:"17", attitude:"worried"}, {name: "Mercutio", age:18, attitude: "hot-headed"}
+
+      ]
+
+   },
    :capulet => {
       :patriarch => {
+          name: "lord capulet", age: "50",
         #your key/value pairs here
         },
       :matriarch => {
         #your key/value pairs here
+        name: "lady capulet", age:"51"
         },
       :heroine => {
+            name:"juliet" , age: "15", status: "alive"
         #your key/value pairs here
         },
-      :heroine_friends => []
-   }
-  }
+      :heroine_friends => [
+        {name: "Steven", age: "30", attitude: "confused"},{name: "Nurse", age: "44", attitude: "worried"}
 
-  
+      ]
+   }
+
+}
+
 end
